a8d0309bbef5ede53422597d9b3cc4e1b5ec5269
Jack Buro <jackkburo@gmail.com>
2010-12-05 16:14:07 +0200

    Finished user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,12 +5,18 @@ class UsersController < ApplicationController
     @title = @user.name
   end
   
-  
-  
   def new
     @user = User.new
     @title = "Sign up"
   end
   
-
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      redirect_to(@user, :notice  => 'Welcome to the sample App!')
+    else
+      @title = "Sign up"
+      render 'new'
+    end
+  end
 end
